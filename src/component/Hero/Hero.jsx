33cd558602b6ccbf4d01e89bf17dd79aba36204f
Guendouz mehdi <mahdign12@gmail.com>
2023-11-17 11:14:52 +0000

    portfolio image

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,59 +1,60 @@
-import React , {useState, useEffect} from 'react';
-import "./Hero.scss"
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Hero.scss";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
+  const words = ["Front-End", "Back-End", "Full Stack"];
 
-    const words = ["Front-End", "Back-End", "Full Stack"];
+  const TypingEffect = ({ words }) => {
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [currentWord, setCurrentWord] = useState("");
 
-    const TypingEffect = ({words}) => {
-        const [currentWordIndex, setCurrentWordIndex] = useState(0);
-        const [currentWord, setCurrentWord] = useState("");
-      
-        useEffect(() => {
-            let currentIndex = 0;
-            let word = ''
-            const intervalId = setInterval(() => {
-              word = word + words[currentWordIndex][currentIndex]
-              setCurrentWord(word);
-              currentIndex++;
-              if (currentIndex === words[currentWordIndex].length) {
-                clearInterval(intervalId);
-                setTimeout(() => {
-                  setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-                  setCurrentWord("");
-                }, 1000);
-              }
-            }, 200);
-        
-            return () => clearInterval(intervalId);
-          }, [currentWordIndex, words]);
-        
-          return <span className='typing'>{currentWord}</span>;
-      };
+    useEffect(() => {
+      let currentIndex = 0;
+      let word = "";
+      const intervalId = setInterval(() => {
+        word = word + words[currentWordIndex][currentIndex];
+        setCurrentWord(word);
+        currentIndex++;
+        if (currentIndex === words[currentWordIndex].length) {
+          clearInterval(intervalId);
+          setTimeout(() => {
+            setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+            setCurrentWord("");
+          }, 1000);
+        }
+      }, 200);
 
+      return () => clearInterval(intervalId);
+    }, [currentWordIndex, words]);
 
+    return <span className="typing">{currentWord}</span>;
+  };
 
-
-
-    return (
-
-
-        <div className='hero-container' >
-            <div className="left" data-aos="fade-right">
-                <h1>Hi!! I AM Mehdi your <br className='break-line' /> <TypingEffect words={words} /> <br /><span className='typing'>Web Developer</span></h1>
-                <p>Crafting digital experiences with passion and <br /> precision</p>
-                <Link to={"/contacts"}>Contact me !!</Link>
-            </div>
-            <div className="right" data-aos="fade-left">
-                <img src="./images/portfolioTest.JPG" alt="" />
-                <div className='card'>
-                    <div className="square"></div>
-                    <div className="content">currently open to <span>work</span></div>
-                </div>
-            </div>
+  return (
+    <div className="hero-container">
+      <div className="left" data-aos="fade-right">
+        <h1>
+          Hi!! I AM Mehdi your <br className="break-line" />{" "}
+          <TypingEffect words={words} /> <br />
+          <span className="typing">Web Developer</span>
+        </h1>
+        <p>
+          Crafting digital experiences with passion and <br /> precision
+        </p>
+        <Link to={"/contacts"}>Contact me !!</Link>
+      </div>
+      <div className="right" data-aos="fade-left">
+        <img src="./images/mehdi.jpg" alt="" />
+        <div className="card">
+          <div className="square"></div>
+          <div className="content">
+            currently open to <span>work</span>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
